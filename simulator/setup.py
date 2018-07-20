2d1731985ae22eafc4263d0ff012b83e0bd0b688
Adam Ludvik <ludvik@bitwise.io>
2018-07-20 14:49:40 -0500

    Update poet-simulator packaging
    Signed-off-by: Adam Ludvik <ludvik@bitwise.io>

--- simulator/setup.py
+++ simulator/setup.py
@@ -31,18 +31,15 @@ data_files = [
 
 setup(name='sawtooth-poet-simulator',
       version=subprocess.check_output(
-          ['../../../bin/get_version']).decode('utf-8').strip(),
+          ['../bin/get_version']).decode('utf-8').strip(),
       description='Sawtooth PoET Simulator Enclave',
       author='Hyperledger Sawtooth',
-      url='https://github.com/hyperledger/sawtooth-core',
+      url='https://github.com/hyperledger/sawtooth-poet',
       packages=find_packages(),
       install_requires=[
           'cryptography>=1.7.1',
-          'protobuf',
           'sawtooth-poet-common',
-          'sawtooth-poet-simulator',
           'sawtooth-signing',
-          'sawtooth-sdk',
       ],
       data_files=data_files,
       entry_points={})
