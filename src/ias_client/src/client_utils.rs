fc0db9c29d216b3ac7e147fe254c882e0270eabc
S m, Aruna <aruna.s.m@intel.com>
2018-11-22 16:33:35 +0530

    IAS client and Http/s client utility
    1) IAS client to get signature revocation list given gid as input.
    2) Generic http/s utility functions such as reading response body
    and header, reading body as string, getting a http/s client to
    connect to both http and https URI.
    
    Signed-off-by: S m, Aruna <aruna.s.m@intel.com>

---
+++ src/ias_client/src/client_utils.rs
@@ -0,0 +1,360 @@
+/*
+ Copyright 2018 Intel Corporation
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+------------------------------------------------------------------------------
+*/
+
+extern crate hyper_tls;
+extern crate native_tls;
+extern crate tokio_core;
+
+use futures::{future,
+              future::Future,
+              stream::Stream};
+use hyper::{Body,
+            Client,
+            client::{HttpConnector,
+                     ResponseFuture},
+            Error,
+            header::HeaderMap,
+            StatusCode};
+use self::hyper_tls::HttpsConnector;
+use self::native_tls::{Certificate,
+                       TlsConnector};
+use self::tokio_core::reactor::Core;
+use std::{error,
+          fmt};
+
+/// Custom error for client utils
+#[derive(Debug, Clone)]
+pub struct ClientError;
+
+impl fmt::Display for ClientError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "invalid value found")
+    }
+}
+
+impl error::Error for ClientError {
+    fn cause(&self) -> Option<&error::Error> {
+        None
+    }
+}
+
+/// read_response_future() would return the ClientResponse which has ```hyper::Body``` and
+/// ```hyper::header::HeaderMap```
+///
+/// Methods to read Body does consume, ideally we should access members through getters
+#[derive(Debug)]
+pub struct ClientResponse {
+    pub body: Body,
+    pub header_map: HeaderMap,
+}
+
+/// Function to get a http and https compatible client to connect to remote URI.
+///
+/// Accepts certificate to be trusted as byte array. Returns a ```hyper::Client``` object that
+/// can be used to connect to a URI having prefix either http or https.
+pub fn get_client(
+    pem_cert: &[u8]
+) -> Result<Client<HttpsConnector<HttpConnector>, Body>, ClientError>
+{
+    let cert = match Certificate::from_pem(pem_cert) {
+        Ok(cert_contents) => cert_contents,
+        Err(error) => {
+            error!("Error creating certificate object from input bytes: {}", error);
+            return Err(ClientError);
+        }
+    };
+    // trust the supplied root certificate
+    let tls_connector = match TlsConnector::builder().add_root_certificate(cert).build() {
+        Ok(tls_connector_built) => tls_connector_built,
+        Err(error) => {
+            error!("Unable to build TLS connector; More info: {}", error);
+            return Err(ClientError);
+        }
+    };
+    let mut http = HttpConnector::new(1);
+    // do not enforce http only URI, we are using TlsConnector to build HttpsConnector for https URI
+    http.enforce_http(false);
+    let https = HttpsConnector::from((http, tls_connector));
+    // build a client to allow both http and https URI formats
+    Ok(Client::builder().build::<_, Body>(https))
+}
+
+/// Function to read ```hyper::client::ResponseFuture``` (return values of .request(), .get(), .post()
+/// etc functions from hyper library).
+///
+/// Returns result ClientResponse and ClientError.
+/// This is a blocking call. A ```tokio_core``` runner instance is created to block until
+/// ```ResponseFuture``` is complete.
+pub fn read_response_future(
+    response_fut: ResponseFuture
+) -> Result<ClientResponse, ClientError> {
+    let future_response = response_fut
+        // 'then' waits for future_response to be ready and calls the closure supplied here on
+        // Result of evaluated future. Response object is ready when closure is called.
+        .then(move |response_obj| {
+            match response_obj {
+                Ok(response) => {
+                    debug!("Received response result code: {}", response.status());
+                    if response.status() != StatusCode::OK {
+                        error!("Response status is not successful: {}", response.status());
+                        return Err(ClientError);
+                    }
+                    // Borrow response headers, to be passed in ClientResponse
+                    let header_map = response.headers().to_owned();
+                    let body = response.into_body();
+                    let client_response = ClientResponse {
+                        body,
+                        header_map,
+                    };
+                    return Ok(client_response);
+                }
+                Err(error) => {
+                    error!("Error occurred while waiting for the ResponseFuture {}", error);
+                    return Err(ClientError);
+                }
+            }
+        });
+    // Create a runner instance for evaluating ResponseFuture
+    let mut runner = Core::new().expect("Error creating core instance");
+    // run() method blocks for future_response
+    runner.run(future_response)
+}
+
+/// Function to read ```hyper::Body``` (body) as string.
+///
+/// Returns result of ```String``` and ```ClientError```.
+/// This is a blocking call. Body is streamed and collected as vector, which later is converted to
+/// string representation.
+pub fn read_body_as_string(
+    body: Body
+) -> Result<String, ClientError> {
+    return body.fold(Vec::new(), |mut vector, chunk| {
+        vector.extend_from_slice(&chunk[..]);
+        future::ok::<_, Error>(vector)
+    })
+        // 'then' evaluates Future to Result. Note that body should be available already.
+        // Construct a Result of string to be returned when body is available.
+        .then(move |body_as_byte_vector| {
+            match body_as_byte_vector {
+                Ok(byte_vector) => {
+                    let body = String::from_utf8(byte_vector)
+                        .expect("Error reading body byte stream as string");
+                    return Ok(body);
+                }
+                Err(error) => {
+                    error!("Error reading body as string {}", error);
+                    return Err(ClientError);
+                }
+            }
+        })
+        // Wait for completion of task assigned to then
+        .wait();
+}
+
+#[cfg(test)]
+mod tests {
+    use hyper::{header::{HeaderName,
+                         HeaderValue},
+                Response,
+                Server,
+                service::service_fn_ok,
+                Uri};
+    use std::{net::{Ipv4Addr,
+                    SocketAddr,
+                    SocketAddrV4},
+              str::FromStr,
+              sync::atomic::{ATOMIC_BOOL_INIT,
+                             AtomicBool,
+                             Ordering::SeqCst},
+              thread};
+    use super::*;
+    use tokio::runtime::Runtime;
+
+    // Variable so that server is not trying to bind again
+    static IS_INITIALIZED: AtomicBool = ATOMIC_BOOL_INIT;
+    lazy_static! {
+        // Prefixed string values for asserting
+        static ref RANDOM_STRING: String = "This string is expected in body".to_string();
+        static ref VALID_HEADER_KEY: String = "ValidHeader".to_string();
+        static ref VALID_HEADER_VALUE: String = "ValidHeaderValue".to_string();
+        static ref INVALID_HEADER_KEY: String = "InvalidHeader".to_string();
+        static ref INVALID_HEADER_VALUE: String = "InvalidHeaderValue".to_string();
+    }
+
+    #[test]
+    fn test_read_body_as_string() {
+        let body_composed = Body::from(RANDOM_STRING.clone());
+        let what_is_read_from_body = read_body_as_string(body_composed)
+            .expect("Error reading body as string");
+        assert_eq!(what_is_read_from_body, RANDOM_STRING.clone());
+    }
+
+    #[test]
+    fn test_read_response_body_as_string() {
+        if IS_INITIALIZED.load(SeqCst) == false {
+            mock_setup_server();
+        }
+        let client = Client::new();
+        let address = "http://127.0.0.1:".to_string().to_owned() + "8080";
+        let future_response = client.get(address.parse::<Uri>()
+            .expect("Error converting string to Uri"));
+        let what_is_read_from_response = read_response_future(future_response)
+            .expect("Error reading response");
+        let body = what_is_read_from_response.body;
+        let what_is_read_from_body = read_body_as_string(body)
+            .expect("Error reading body as string");
+        assert_eq!(what_is_read_from_body, RANDOM_STRING.clone());
+    }
+
+    #[test]
+    fn test_read_response_body_as_string_with_header() {
+        if IS_INITIALIZED.load(SeqCst) == false {
+            mock_setup_server();
+        }
+        let client = Client::new();
+        let address = "http://127.0.0.1:".to_string().to_owned() + "8080";
+        let future_response = client.get(address.parse::<Uri>()
+            .expect("Error converting string to Uri"));
+        let what_is_read_from_response = read_response_future(future_response)
+            .expect("Error reading response");
+        let body = what_is_read_from_response.body;
+        let what_is_read_from_body = read_body_as_string(body)
+            .expect("Error reading body as string");
+        assert_eq!(what_is_read_from_body, RANDOM_STRING.clone());
+        let header_map_read = what_is_read_from_response.header_map;
+        assert!(header_map_read.contains_key(VALID_HEADER_KEY.clone()));
+        assert_eq!(header_map_read.get(VALID_HEADER_KEY.clone())
+                       .expect("Error reading header value"),
+                   VALID_HEADER_VALUE.clone().as_str());
+    }
+
+    #[test]
+    fn test_read_response_body_as_string_with_invalid_header() {
+        if IS_INITIALIZED.load(SeqCst) == false {
+            mock_setup_server();
+        }
+        let client = Client::new();
+        let address = "http://127.0.0.1:".to_string().to_owned() + "8080";
+        let future_response = client.get(address.parse::<Uri>()
+            .expect("Error converting string to Uri"));
+        let what_is_read_from_response = read_response_future(future_response)
+            .expect("Error reading response");
+        let body = what_is_read_from_response.body;
+        let what_is_read_from_body = read_body_as_string(body)
+            .expect("Error reading body as string");
+        assert_eq!(what_is_read_from_body, RANDOM_STRING.clone());
+        let header_map_read = what_is_read_from_response.header_map;
+        assert_eq!(header_map_read.contains_key(INVALID_HEADER_KEY.clone()), false);
+    }
+
+    #[test]
+    fn test_get_client() {
+        if IS_INITIALIZED.load(SeqCst) == false {
+            mock_setup_server();
+        }
+        let cert = "-----BEGIN CERTIFICATE-----
+MIICaTCCAdKgAwIBAgIJAItOJYg0b5+lMA0GCSqGSIb3DQEBCwUAMEwxCzAJBgNV
+BAYTAklOMQswCQYDVQQIDAJLQTELMAkGA1UEBwwCQkExDjAMBgNVBAoMBUludGVs
+MRMwEQYDVQQLDApCbG9ja2NoYWluMB4XDTE4MTAyNDEyNDEwMFoXDTE5MTAyNDEy
+NDEwMFowTDELMAkGA1UEBhMCSU4xCzAJBgNVBAgMAktBMQswCQYDVQQHDAJCQTEO
+MAwGA1UECgwFSW50ZWwxEzARBgNVBAsMCkJsb2NrY2hhaW4wgZ8wDQYJKoZIhvcN
+AQEBBQADgY0AMIGJAoGBAM/uqnOiEKD/TcshpNOr8/hD/7WAvRDcPb7IFwzoaS1t
+NaheDh2W4EGd8jLPmknYGSHUL8ust2dZXTrMXxHGZWpbnEA15dboJptXEem5XoOG
+04NYw6sr/r7Bv9pi1Y34JDu7vcwdK29XHSI58msGEeU3RlPB3fuWVrw6yV2oO1FZ
+AgMBAAGjUzBRMB0GA1UdDgQWBBTOVWh5+ajCi7Xyq/cLDLCKVqm6/DAfBgNVHSME
+GDAWgBTOVWh5+ajCi7Xyq/cLDLCKVqm6/DAPBgNVHRMBAf8EBTADAQH/MA0GCSqG
+SIb3DQEBCwUAA4GBABg38E87twLUOKKwfS+bKHnp35x4eoD3Jg/e82zusADdNg6/
+FG7njB5GTDXQhfdcjTo33+pzxr38jSxSEK0g9EJ3+nWqX1SFsKl7m38GOuhjzlxc
+fL464/eRImnAtIPsDe+bywGUc5mq/EEiJ+90jXP1LAWgUk2Ip5Hl0BeEM34U
+-----END CERTIFICATE-----";
+        let client = get_client(cert.as_ref())
+            .expect("Error creating the client instance");
+        let address = "http://127.0.0.1:".to_string().to_owned() + "8080";
+        let future_response = client.get(address.parse::<Uri>()
+            .expect("Error converting string to Uri"));
+        let what_is_read_from_response = read_response_future(future_response)
+            .expect("Error reading body as string");
+        let body = what_is_read_from_response.body;
+        let what_is_read_from_body = read_body_as_string(body)
+            .expect("Error reading body as string");
+        assert_eq!(what_is_read_from_body, RANDOM_STRING.clone());
+    }
+
+    #[test]
+    fn test_not_ok_read_response_body_with_string() {
+        mock_setup_bad_server();
+        let client = Client::new();
+        let address = "http://127.0.0.1:".to_string().to_owned() + "8081";
+        let future_response = client.get(address.parse::<Uri>()
+            .expect("Error converting string to Uri"));
+        match read_response_future(future_response) {
+            Ok(_response_body) => assert!(false),
+            Err(_error) => assert!(true),
+        };
+    }
+
+    fn mock_setup_server() {
+        IS_INITIALIZED.store(true, SeqCst);
+        let loopback_addr = Ipv4Addr::new(127, 0, 0, 1);
+        // TODO: Use random port here
+        let socket_addr: SocketAddr = SocketAddr::from(SocketAddrV4::new(loopback_addr, 8080));
+        let new_service = move || {
+            service_fn_ok(|_| {
+                let mut response = Response::new(Body::from(RANDOM_STRING.clone()));
+                response.headers_mut().insert(HeaderName::from_str(VALID_HEADER_KEY.as_str())
+                                                  .expect("Error converting string to header name"),
+                                              HeaderValue::from_str(VALID_HEADER_VALUE.as_str())
+                                                  .expect("Error converting string to header value"));
+                response
+            })
+        };
+        let server = Server::bind(&socket_addr)
+            .serve(new_service)
+            .map_err(|e| panic!("server error: {}", e));
+
+        // TODO: Force this thread to close after test case ends
+        thread::spawn(|| {
+            let mut handler = Runtime::new().expect("Error creating runner instance");
+            handler.block_on(server).expect("Error blocking on the service")
+        });
+    }
+
+    fn mock_setup_bad_server() {
+        let loopback_addr = Ipv4Addr::new(127, 0, 0, 1);
+        // TODO: Use random port here
+        let socket_addr: SocketAddr = SocketAddr::from(SocketAddrV4::new(loopback_addr, 8081));
+        let new_service = move || {
+            service_fn_ok(|_| {
+                let mut response = Response::new(Body::from(RANDOM_STRING.clone()));
+                // Return any status >= 400
+                // This is to simulate that server is responding bad
+                *response.status_mut() = StatusCode::from_u16(400)
+                    .expect("Error reading status code from integer");
+                response
+            })
+        };
+        let server = Server::bind(&socket_addr)
+            .serve(new_service)
+            .map_err(|e| panic!("server error: {}", e));
+
+        // TODO: Force this thread to close after test case ends
+        thread::spawn(|| {
+            let mut handler = Runtime::new().expect("Error creating runner instance");
+            handler.block_on(server).expect("Error blocking on the service")
+        });
+    }
+}
---
+++ src/ias_client/src/ias_client.rs
@@ -0,0 +1,238 @@
+/*
+ Copyright 2018 Intel Corporation
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+------------------------------------------------------------------------------
+*/
+
+extern crate serde;
+
+use client_utils::{ClientError,
+                   ClientResponse,
+                   get_client,
+                   read_response_future};
+use hyper::{Body,
+            header,
+            header::HeaderValue,
+            Method,
+            Request,
+            Uri};
+use serde_json;
+use std::{collections::HashMap,
+          str,
+          time::Duration};
+
+/// Structure for storing IAS connection information
+#[derive(Debug, Clone)]
+pub struct IasClient {
+    // IAS URL to connect to
+    ias_url: String,
+    // Root cert to be trusted
+    spid_cert: Vec<u8>,
+    // Timeout for the client requests in seconds
+    timeout: Duration,
+}
+
+const SIGRL_LINK: &str = "/attestation/sgx/v2/sigrl";
+const AVR_LINK: &str = "/attestation/sgx/v2/report";
+// Note: Structure can be used for serialization and deserialization, but it won't skip null values
+const ISV_ENCLAVE_QUOTE: &str = "isvEnclaveQuote";
+const PSE_MANIFEST: &str = "pseManifest";
+const NONCE: &str = "nonce";
+// timeout constants
+const DEFAULT_TIMEOUT_SECS: u64 = 300;
+const DEFAULT_TIMEOUT_NANO_SECS: u32 = 0;
+
+/// Implement how the IasClient is going to be used
+impl IasClient {
+    /// default constructor for IasClient, remember to use setters later
+    pub fn default() -> Self {
+        IasClient {
+            ias_url: String::new(),
+            spid_cert: [].to_vec(),
+            timeout: Duration::new(DEFAULT_TIMEOUT_SECS,
+                                   DEFAULT_TIMEOUT_NANO_SECS),
+        }
+    }
+
+    /// constructor for IasClient
+    pub fn new(url: String, cert: Vec<u8>, time: Option<u64>) -> Self {
+        IasClient {
+            ias_url: url,
+            spid_cert: cert,
+            timeout: Duration::new(time.unwrap_or(DEFAULT_TIMEOUT_SECS),
+                                   DEFAULT_TIMEOUT_NANO_SECS),
+        }
+    }
+
+    /// Setters for IasClient structure
+    pub fn set_ias_url(&mut self, url: String) {
+        self.ias_url = url;
+    }
+
+    pub fn set_spid_cert(&mut self, cert: Vec<u8>) {
+        self.spid_cert = cert;
+    }
+
+    pub fn set_timeout(&mut self, timeout: Duration) {
+        self.timeout = timeout;
+    }
+
+    /// Get request to receive signature revocation list for input Group ID (gid). Accepts
+    /// optional 'gid' and optional 'api_path' as input. Optional 'gid' field is used for the
+    /// case of IAS Proxy server, which receives request with 'gid' appended already.
+    ///
+    /// return: A ClientResponse object containing the following:
+    ///     Body of the response has 'signature revocation list', the body of the response from IAS.
+    ///     Header of the response has nothing.
+    pub fn get_signature_revocation_list(
+        &self,
+        gid: Option<&str>,
+        api_path: Option<&str>,
+    ) -> Result<ClientResponse, ClientError> {
+
+        // Path to get SigRL from
+        let mut final_path = String::new();
+        final_path.push_str(self.ias_url.as_str());
+        // Received REST path if any
+        let received_path = match api_path {
+            Some(path_present) => path_present,
+            _ => SIGRL_LINK,
+        };
+        final_path.push_str(received_path);
+        // Append gid to the path if present
+        let received_gid = match gid {
+            Some(gid_present) => gid_present,
+            _ => "",
+        };
+        final_path.push_str(received_gid);
+        let url = final_path.parse::<Uri>().expect("Error constructing URI from string");
+        debug!("Fetching SigRL from: {}", url);
+
+        // Send request to get SigRL
+        let client = get_client(self.spid_cert.as_slice())
+            .expect("Eror creating http/s client");
+        // TODO: Add logic for request timeout
+        let response_fut = client.get(url);
+        read_response_future(response_fut)
+    }
+
+    /// Post request to send Attestation Enclave Payload and get response having Attestation
+    /// Verification Report. Accepts quote and optional values pse_manifest, nonce as input.
+    ///
+    /// return: A ClientResponse object containing the following:
+    ///     Body of the response has 'attestation verification report', the body (JSON) of the
+    ///         response from ISA.
+    ///     Header of the response has 'signature', the base 64-encoded RSA-SHA256 signature of the
+    ///         response body (aka, AVR) using the report key.
+    pub fn post_verify_attestation(
+        &self,
+        quote: &[u8],
+        manifest: Option<&str>,
+        nonce: Option<u64>,
+    ) -> Result<ClientResponse, ClientError> {
+
+        // REST API to connect to for getting AVR
+        let mut final_path = String::new();
+        final_path.push_str(self.ias_url.as_str());
+        final_path.push_str(AVR_LINK);
+        let url = final_path.parse::<Uri>().expect("Error constructing URI from string");
+        debug!("Posting attestation verification request to: {}", url);
+
+        // Construct AEP, request parameter
+        // Note: Replace following HashMap with a structure if Integration test with IAS succeeds
+        // with keys in request json with empty value. With following code, we are avoiding even
+        // addition of keys in request json.
+        let mut request_aep: HashMap<String, String> = HashMap::new();
+        request_aep.insert(String::from(ISV_ENCLAVE_QUOTE),
+                           str::from_utf8(quote)
+                               .expect("Error occurred when converting quote to string")
+                               .to_owned());
+        // Optional manifest, add to request param if present
+        if manifest.is_some() {
+            request_aep.insert(String::from(PSE_MANIFEST),
+                               manifest.unwrap().to_owned());
+        }
+        // Optional nonce, add to request param if present
+        if nonce.is_some() {
+            request_aep.insert(String::from(NONCE),
+                               format!("{}", nonce.unwrap()).to_owned());
+        }
+
+        // Construct hyper's request to be sent
+        let mut req = Request::new(
+            Body::from(
+                serde_json::to_string(&request_aep)
+                    .expect("Error occurred during AEP serialization")));
+        *req.method_mut() = Method::POST;
+        *req.uri_mut() = url.clone();
+        req.headers_mut().insert(
+            header::CONTENT_TYPE,
+            HeaderValue::from_static("application/json"),
+        );
+        debug!("Posting attestation evidence payload: {:#?}", request_aep);
+
+        // Send request to get AVR
+        let client = get_client(self.spid_cert.as_slice())
+            .expect("Error creating http client");
+        let response_fut = client.request(req);
+        // TODO: Add logic for request timeout
+        read_response_future(response_fut)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const DEFAULT_DURATION: u64 = 300;
+    const DUMMY_DURATION: u64 = 0;
+    const DEFAULT_URL: &str = "";
+    const DUMMY_URL: &str = "dummy.url";
+    lazy_static! {
+        static ref DEFAULT_CERT: Vec<u8> = [].to_vec();
+        static ref DUMMY_CERT: Vec<u8> = "random_byte_contents".as_bytes().to_vec();
+    }
+
+    #[test]
+    fn test_default_ias_client_creation() {
+        let default_client = IasClient::default();
+        assert_eq!(default_client.ias_url, DEFAULT_URL.clone());
+        assert_eq!(default_client.spid_cert.len(), DEFAULT_CERT.len());
+        assert_eq!(default_client.timeout.as_secs(), DEFAULT_DURATION);
+    }
+
+    #[test]
+    fn test_new_ias_client_creation() {
+        let new_ias_client = IasClient::new(
+            DUMMY_URL.clone().to_string(),
+            DUMMY_CERT.clone(),
+            Option::from(DUMMY_DURATION));
+        assert_eq!(new_ias_client.ias_url, DUMMY_URL.clone());
+        assert_eq!(new_ias_client.spid_cert.len(), DUMMY_CERT.len());
+        assert_eq!(new_ias_client.timeout.as_secs(), DUMMY_DURATION);
+    }
+
+    #[test]
+    fn test_new_ias_client_with_assignment() {
+        let mut default_client = IasClient::default();
+        default_client.set_ias_url(DUMMY_URL.clone().to_string());
+        default_client.set_spid_cert(DUMMY_CERT.clone());
+        default_client.set_timeout(Duration::new(DUMMY_DURATION, 0));
+        assert_eq!(default_client.ias_url, DUMMY_URL.clone());
+        assert_eq!(default_client.spid_cert.len(), DUMMY_CERT.len());
+        assert_eq!(default_client.timeout.as_secs(), DUMMY_DURATION);
+    }
+    // Reading from response / body, reading of headers are handled in client_utils.rs
+    // Please find the file for unit tests on those
+}
